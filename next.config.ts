cadb50e2bcb4b9b3a6d7e3361c85c769a842998d
kaannakiin <kaan.akin@std.yildiz.edu.tr>
2024-10-23 19:17:59 +0300

    added_mantine

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  sassOptions: {
+    prependData: `@import "./_mantine.scss";`,
+  },
 };
 
 export default nextConfig;
